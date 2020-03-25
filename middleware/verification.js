9fdb25a8addd56b08bec6003f69460b320c3c8ce
Leo Reuter <leo.reuter@i22.de>
2020-03-25 22:17:49 +0100

    update: verification update

--- middleware/verification.js
+++ middleware/verification.js
@@ -1,3 +1,3 @@
 export default function (req, res) {
-  return res.send('0ueRc1aGXuitct5y-xf3rrdNgt0RMHU06j2W-4RYYuQ.kGnBEFplO3j5RXA2BgAAsUhLOaQGzqAyIt8CtmHPraI')
+  return res.end('0ueRc1aGXuitct5y-xf3rrdNgt0RMHU06j2W-4RYYuQ.kGnBEFplO3j5RXA2BgAAsUhLOaQGzqAyIt8CtmHPraI')
 }

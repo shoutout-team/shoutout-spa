b5d3fbdb78473389c71fbfaa754c9267b9dc4033
Henry Steinhauer <henry.steinhauer@i22.de>
2020-03-28 03:02:57 +0100

    edit structure and style on donation page components

--- components/DonateElement.vue
+++ components/DonateElement.vue
@@ -1,20 +1,7 @@
 <template>
-  <v-flex class="justify-center my-12">
-    <v-btn
-      v-if="paymentType !== 'bank'"
-      icon
-      width="100%"
-      link
-      :href="paymentInfo[paymentType]"
-      target="_blank"
-    >
-      <v-img class="image" :alt="paymentType" :src="images[paymentType]" />
-    </v-btn>
-    <p v-if="paymentType === 'bank'" class="pt-7 text-center">
-      Name: <nobr>Max Mustermann</nobr> <br>
-      IBAN: <nobr>0011 1111 3333 3322</nobr>
-    </p>
-  </v-flex>
+  <a>
+    <v-img width="100%" class="image" :alt="payment.type" :src="images[payment.type]" />
+  </a>
 </template>
 
 <script>
@@ -22,32 +9,21 @@ import { mdiArrowRightThick } from '@mdi/js'
 
 export default {
   props: {
-    paymentInfo: {
+    payment: {
       type: Object,
       default: () => {}
-    },
-    paymentType: {
-      type: String,
-      default: ''
     }
+  },
+  mounted () {
+    console.log(this.payment)
   },
   data () {
     return {
       mdiArrowRightThick,
-      paymentHeadlines: {
-        paypal: 'PayPal',
-        gofoundme: 'GoFundMe',
-        bank: 'Banküberweisung'
-      },
       images: {
         paypal: require('~/assets/icon-paypal.png'),
         gofoundme: require('~/assets/icon-gofundme.png')
       }
-    }
-  },
-  computed: {
-    distanceWithUnit () {
-      return this.distance > 999 ? `${this.distance / 1000}km` : `${this.distance}m`
     }
   }
 }

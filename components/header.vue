e8cd28c1bbf73d87731d1b55b8a9aa897f0563e9
Leo Reuter <leo.reuter@i22.de>
2020-04-01 09:23:34 +0200

    hotfix header, footer

--- components/header.vue
+++ components/header.vue
@@ -2,14 +2,14 @@
   <v-container class="header">
     <v-row justify="center" align="center">
       <v-col cols="5" sm="3" xl="5">
-        <nuxt-link to="/">
+        <n-link to="/">
           <img src="~/assets/logo.png" alt="Shoutout!" class="header__logo">
-        </nuxt-link>
+        </n-link>
       </v-col>
       <v-col cols="7" sm="9" xl="5" class="text-right">
-        <v-btn class="font-weight-bold" tile text nuxt to="/overview">VIERTELFREUNDE</v-btn>
-        <v-btn class="font-weight-bold" tile text nuxt :to="user ? '/edit-company' : 'signup'"> {{ user ? 'Mein Profil' : 'VIERTELHELDEN' }}</v-btn>
-        <v-btn class="font-weight-bold" tile text nuxt to="/about-us">Über uns</v-btn>
+        <n-link class="header__link px-3 text-uppercase mb-2 font-weight-bold" to="/overview">Viertelfreunde</n-link>
+        <n-link class="header__link px-3 text-uppercase mb-2 font-weight-bold" :to="user ? '/edit-company' : 'signup'"> {{ user ? 'Mein Profil' : 'Viertelhelden' }}</n-link>
+        <n-link class="header__link px-3 text-uppercase mb-2 font-weight-bold" to="/about-us">Über uns</n-link>
       </v-col>
     </v-row>
   </v-container>
@@ -31,6 +31,29 @@ export default {
 
   &__logo {
     width: 120px;
+  }
+
+  &__link {
+    position: relative;
+    font-size: .875rem;
+    letter-spacing: 0.75px;
+    display: inline-block;
+    text-decoration: none;
+    white-space: nowrap;
+
+    &.nuxt-link-active {
+      &::after {
+        display: block;
+        content: '';
+        position: absolute;
+        bottom: -6px;
+        left: 0;
+        width: 100%;
+        height: 7px;
+        background-image: url('~@/assets/faq-strich-helfer.png');
+        background-size: 100%;
+      }
+    }
   }
 }
 </style>

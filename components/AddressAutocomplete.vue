6791c5b7b10cff469356661ea33777716bec02e6
Henry Steinhauer <henry.steinhauer@i22.de>
2020-04-01 23:55:08 +0200

    remove logs

--- components/AddressAutocomplete.vue
+++ components/AddressAutocomplete.vue
@@ -39,10 +39,8 @@ export default {
   },
   watch: {
     starting_address_obj () {
-      console.log(this.startingAddressAutocomplete, 'autocomplete')
       const elements = ['locality', 'route', 'postal_code', 'street_number']
       if (!this.starting_address_obj.place.address_components) { return }
-      console.log('geo', this.starting_address_obj)
       const geolocation = this.starting_address_obj.place.address_components.filter(e => elements.includes(e.types[0]))
       const location = {
         latitude: this.starting_address_obj.place.geometry.location.lat(),

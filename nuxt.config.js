fe8da6c98b6ff86aa97084cb2b93e6c157f71ee2
Henry Steinhauer <henry.steinhauer@i22.de>
2020-04-02 12:21:50 +0200

    create endpoint

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,8 @@ require('dotenv').config()
 
 export default {
   serverMiddleware: [
-    'redirect-ssl'
+    'redirect-ssl',
+    { path: '/loaderio-6675f0444ad2b4bc8baefb7958ad98d4', handler: '~/middleware/verification.js' }
   ],
   mode: 'universal',
 
